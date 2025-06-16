04eda110d224bee6bd60cd7caa2c887081704b80
BurakYaba <brkyaba@gmail.com>
2025-06-16 17:33:29 +0300

    Removed Unnecessary Dependencies

--- next.config.js
+++ next.config.js
@@ -6,8 +6,6 @@ const nextConfig = {
       bodySizeLimit: '4mb',
     },
   },
-  // Add transpilePackages for Three.js
-  transpilePackages: ['three', '@react-three/drei', '@react-three/fiber'],
   // The functions configuration is not valid in next.config.js
   // It should only be in vercel.json
 };
